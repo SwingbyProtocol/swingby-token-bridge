32ec17d6b60fa883be70127f1c8902da4ae3ea95
Rubén <ruben@swingby.network>
2021-04-14 17:43:43 +0200

    fix: Compilation errors in API

--- src/pages/api/v1/process/transactions/[network].tsx
+++ src/pages/api/v1/process/transactions/[network].tsx
@@ -1,17 +1,19 @@
-import { createEndpoint, getStringParam } from '../../../../../modules/server__api-endpoint';
-import { isValidNetworkId, NETWORK_IDS } from '../../../../../modules/onboard';
+import { StatusCodes } from 'http-status-codes';
+
+import { createEndpoint } from '../../../../../modules/server__api-endpoint';
 
 export default createEndpoint({
   isSecret: true,
   fn: async ({ req, res }) => {
-    const networkId = +getStringParam({
-      req,
-      from: 'query',
-      name: 'network',
-      oneOf: NETWORK_IDS.map((it) => `${it}` as const),
-    });
-    if (!isValidNetworkId(networkId)) {
-      throw new Error(`Invalid network ID: ${networkId}`);
-    }
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    // const networkId = +getStringParam({
+    //   req,
+    //   from: 'query',
+    //   name: 'network',
+    //   oneOf: NETWORK_IDS.map((it) => `${it}` as const),
+    // });
+    // if (!isValidNetworkId(networkId)) {
+    //   throw new Error(`Invalid network ID: ${networkId}`);
+    // }
   },
 });
