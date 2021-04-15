c8e254b432517351cdd9c7d9d53fb0d8995bf8b6
Rubén <ruben@swingby.network>
2021-04-15 14:17:58 +0200

    fix: `getBep20CrossChainAllowance()`

--- src/modules/web3/getBep20CrossChainAllowance.tsx
+++ src/modules/web3/getBep20CrossChainAllowance.tsx
@@ -1,12 +1,13 @@
 import Web3 from 'web3';
 import { Big } from 'big.js';
 import { API } from 'bnc-onboard/dist/src/interfaces'; // eslint-disable-line import/no-internal-modules
+import ABI from 'human-standard-token-abi';
 
 import { logger } from '../logger';
-import { fetcher } from '../fetch';
+import { SB_TOKEN_CONTRACT } from '../swingby-token';
 
 import { getOnboardData } from './getOnboardData';
-import { BEP20_CROSS_CHAIN_CONTRACT, BEP20_CROSS_CHAIN_ABI } from './BEP20_CROSS_CHAIN_CONTRACT';
+import { BEP20_CROSS_CHAIN_CONTRACT } from './BEP20_CROSS_CHAIN_CONTRACT';
 
 export const getBep20CrossChainAllowance = async ({
   onboard: onboardParam,
@@ -18,13 +19,11 @@ export const getBep20CrossChainAllowance = async ({
     throw new Error(`Invalid network for BEP20->BEP2 transfers: ${network}`);
   }
 
-  const { address: addressDeposit } = await fetcher<{ address: string }>('/api/v1/address');
   const web3 = new Web3(wallet.provider);
-  const contract = new web3.eth.Contract(
-    BEP20_CROSS_CHAIN_ABI,
-    BEP20_CROSS_CHAIN_CONTRACT[network],
-  );
-  const allowance = await contract.methods.allowance(address, addressDeposit).call();
+  const contract = new web3.eth.Contract(ABI, SB_TOKEN_CONTRACT[network]);
+  const allowance = await contract.methods
+    .allowance(address, BEP20_CROSS_CHAIN_CONTRACT[network])
+    .call();
   logger.debug('Allowance call returned: %s', allowance);
 
   const decimals = await contract.methods.decimals().call();
