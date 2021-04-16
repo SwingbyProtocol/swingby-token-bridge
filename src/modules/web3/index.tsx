52a59af4241729263988537ee0ecfa955778ed96
Rubén <ruben@swingby.network>
2021-04-16 11:36:20 +0200

    feat: Transfer to hot wallet

--- src/modules/web3/index.tsx
+++ src/modules/web3/index.tsx
@@ -1,7 +1,6 @@
 export { getSwingbyBalance } from './getSwingbyBalance';
-export { approveHotWallet } from './approveHotWallet';
-export { getHotWalletAllowance } from './getHotWalletAllowance';
 export { getDestinationNetwork } from './getDestinationNetwork';
 export { getBep20CrossChainAllowance } from './getBep20CrossChainAllowance';
 export { approveBep20CrossChainTransfer } from './approveBep20CrossChainTransfer';
 export { doBep20CrossChainTransfer } from './doBep20CrossChainTransfer';
+export { transferToHotWallet } from './transferToHotWallet';
