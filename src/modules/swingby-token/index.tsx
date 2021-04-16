2d009822c733095a715b617352d8ce941e39ad48
Rubén <ruben@swingby.network>
2021-04-16 15:10:25 +0200

    fix: Upgrade token address on BSC (T)

--- src/modules/swingby-token/index.tsx
+++ src/modules/swingby-token/index.tsx
@@ -2,5 +2,5 @@ export const SB_TOKEN_CONTRACT = {
   1: '',
   5: '0xfcd51b56e65605c33024a9e98a7aadff2e1a15b9',
   56: '0x71de20e0c4616e7fcbfdd3f875d568492cbe4739',
-  97: '0x1d606092b6c14d940bc421884009af9ea6f54c1c',
+  97: '0x423fa1c32541b3d9fc3690e15392185753d44fad',
 } as const;
