df49b6c5c174d235e4030ff7f0242cfd30c01be4
Rubén <ruben@swingby.network>
2021-04-29 21:31:06 +0200

    feat: Added Ethereum token contract address

--- src/modules/swingby-token/index.tsx
+++ src/modules/swingby-token/index.tsx
@@ -1,5 +1,5 @@
 export const SB_TOKEN_CONTRACT = {
-  1: '',
+  1: '0x8287c7b963b405b7b8d467db9d79eec40625b13a',
   5: '0xfcd51b56e65605c33024a9e98a7aadff2e1a15b9',
   56: '0x71de20e0c4616e7fcbfdd3f875d568492cbe4739',
   97: '0x423fa1c32541b3d9fc3690e15392185753d44fad',
