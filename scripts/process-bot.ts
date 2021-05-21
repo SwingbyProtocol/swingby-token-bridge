18bb6fb399052886391af6571e34bbe0e0b6d123
Rubén <ruben@swingby.network>
2021-05-21 16:25:03 +0200

    chore: Process bot should call `/process/team-wallet-balances`

--- scripts/process-bot.ts
+++ scripts/process-bot.ts
@@ -4,6 +4,7 @@ import { server__processTaskSecret } from '../src/modules/server__env';
 
 const NETWORKS = ['ethereum', 'goerli', 'bsc', 'bsct'];
 const TASKS = ['deposits', 'failed-payments', 'payments', 'pending-payments'];
+const GENERIC_TASKS = ['team-wallet-balances'];
 
 NETWORKS.forEach((network) => {
   TASKS.forEach((task) => {
@@ -16,6 +17,15 @@ NETWORKS.forEach((network) => {
   });
 });
 
+GENERIC_TASKS.forEach((task) => {
+  (async () => {
+    const generator = runGenericTask(task);
+    for await (let value of generator) {
+      logger.info(value, 'Got result for generic task %j', task);
+    }
+  })();
+});
+
 async function* runNetworkTask(network: typeof NETWORKS[number], task: typeof TASKS[number]) {
   for (;;) {
     try {
@@ -26,6 +36,22 @@ async function* runNetworkTask(network: typeof NETWORKS[number], task: typeof TA
       yield { network, task, result };
     } catch (e) {
       yield { network, task, err: e as Error };
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+}
+
+async function* runGenericTask(task: typeof GENERIC_TASKS[number]) {
+  for (;;) {
+    try {
+      const result = await fetcher<Record<string, any>>(
+        `https://k8s.skybridge.exchange/swingby-token-bridge/api/v1/process/${task}?secret=${server__processTaskSecret}`,
+      );
+
+      yield { task, result };
+    } catch (e) {
+      yield { task, err: e as Error };
     }
 
     await new Promise((resolve) => setTimeout(resolve, 5000));
