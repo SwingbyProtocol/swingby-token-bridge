9bd01d2ee621b174e25d35e43a4a179a05801005
Rubén <ruben@swingby.network>
2021-07-05 10:30:50 +0200

    feat: Playground opens with query for "Stuck deposits" already open

--- src/pages/api/v1/graphql.tsx
+++ src/pages/api/v1/graphql.tsx
@@ -5,16 +5,54 @@ import { corsMiddleware } from '../../../modules/server__cors';
 import { prisma } from '../../../modules/server__env';
 import { schema } from '../../../modules/model';
 
+const endpoint = '/api/v1/graphql';
+
+const stuckDeposits = `
+  {
+    deposits(
+      where: {
+        network: { in: [ETHEREUM, BSC] }
+        payments: { every: { status: { notIn: [COMPLETED, PENDING] } } }
+        crashes: { none: {} }
+      }
+    ) {
+      edges {
+        node {
+          hash
+          at
+          addressFrom
+          value
+          crashes {
+            reason
+          }
+          payments {
+            hash
+            status
+          }
+        }
+      }
+    }
+  }
+`;
+
 const server = new ApolloServer({
   schema,
-  playground: true,
+  playground: {
+    tabs: [
+      {
+        name: 'Stuck deposits',
+        endpoint,
+        query: stuckDeposits,
+      },
+    ],
+  },
   introspection: true,
   context: {
     prisma,
   },
 });
 
-const handler = server.createHandler({ path: '/api/v1/graphql' });
+const handler = server.createHandler({ path: endpoint });
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   await corsMiddleware({ req, res });
   return handler(req, res);
